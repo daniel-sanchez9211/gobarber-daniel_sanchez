670b01c0101c2b2b0ebaea94ff1b5a670a61381c
dan_sanchez <daniel.sanchez@keepsimple.com.br>
2020-03-22 20:59:05 -0300

    send emails

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: '2525',
+    secure: false,
+    auth: {
+        user: '22ff1172596fce',
+        pass: '8b2a865b43dfa5',
+    },
+    default: {
+        from: 'Daniel Sanchez <noreply@gobarber.com>'
+    },
+}
